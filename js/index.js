e5e70d5aaa645e4bdbd15c3ea199773a3777b11b
EvgeniaMaltseva <evmaltseva74@gmail.com>
2023-03-18 19:40:13 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,63 @@
+const clean = 40000;
+const sing = 22000;
+const summe = clean + sing;
+
+console.log('Ответ : вечерний уборщик, имея голос и педагогическое образование, может зарабатывать ' + summe + ' рублей');
+
+
+let greeting = "Владивосток";
+let number = "2000";
+
+console.log(greeting + '-' + number);
+
+
+const messageTime = 128;
+const photoLike = 254;
+const readVKontakte = 137;
+const watchYoutube = 201;
+const totalTime = (messageTime + photoLike + readVKontakte + watchYoutube) / 60;
+
+console.log('Русалка прокрастинировала' + " " + totalTime + " " + 'часов');
+
+console.log(2 ** 5);
+
+console.log("Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!");
+
+const greetings = "Привет";
+const name = "Кот";
+
+console.log(greetings + "," + " " + name);
+
+/*let time;
+time = 34;
+let time1 = 19;
+let time2 = 9;
+let endtime = 0;
+console.log('Старт поездки. Осталось минут: ' + time);
+console.log('Немного сторис в соцсетях. Осталось минут: ' + time1);
+console.log('Немного не новостей, но событий. Осталось минут: ' + time2);
+console.log('Вы приехали. Добро пожаловать в Москву. Осталось минут: ' + endtime);*/
+
+let time;
+time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+time = 19;
+console.log('Немного сторис в соцсетях. Осталось минут: ' + time);
+time = 9;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву. Осталось минут: ' + time);
+
+
+/*let time;
+time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+console.log('Немного сторис в соцсетях. Осталось минут: ' + (time-15));
+console.log('Немного не новостей, но событий. Осталось минут: ' + (time-25));
+console.log('Вы приехали. Добро пожаловать в Москву. Осталось минут: ' + (time-34));*/
+
+let fahrenheit = 451;
+let celsius = (fahrenheit - 32) / 1.8;
+console.log(fahrenheit + " " + 'градуса по Фаренгейту — это' +" " + celsius + " " + 'градуса по Цельсию.');
+
+
